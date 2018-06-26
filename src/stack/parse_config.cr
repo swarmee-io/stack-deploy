a872cce5b090fb039d541257ff3c89e43f78a683
Source <source@swarmee.io>
2018-06-26 15:40:12 +0000

    implement embedding configs in yaml

--- src/stack/parse_config.cr
+++ src/stack/parse_config.cr
@@ -4,13 +4,16 @@ module Stack
   class Config
     property name
     property file
-    property content
+    property data
     property content_hash
 
-    def initialize(name : String, file : String, content : String, content_hash : String)
+    def initialize(name : String,
+                   file : String,
+                   data : String,
+                   content_hash : String)
       @name = name
       @file = file
-      @content = content
+      @data = data
       @content_hash = content_hash
     end
 
@@ -23,23 +26,32 @@ module Stack
     end
 
     def create(dir : String, stack_name : String)
-      Process.run("docker", get_cmd(stack_name),
-        input: STDIN,
-        output: STDOUT,
-        error: STDERR,
-        chdir: dir)
+      if @data != ""
+        stdin = IO::Memory.new(@data)
+        Process.run("docker", get_cmd(stack_name),
+          input: stdin,
+          output: STDOUT,
+          error: STDERR,
+          chdir: dir)
+      else
+        Process.run("docker", get_cmd(stack_name),
+          input: STDIN,
+          output: STDOUT,
+          error: STDERR,
+          chdir: dir)
+      end
     end
   end
 
   def self.parse_config(name : String, config : YAML::Any)
-    content = ""
+    data = ""
     begin
-      content = config["content"].as_s
+      data = config["data"].as_s
     rescue KeyError
     end
 
-    file = ""
-    if content == ""
+    file = "-"
+    if data == ""
       begin
         file = config["file"].as_s
         # content = File.read(file)
@@ -52,7 +64,7 @@ module Stack
     return Config.new(
       name,
       file,
-      content,
+      data,
       content_hash: "",
     )
   end
